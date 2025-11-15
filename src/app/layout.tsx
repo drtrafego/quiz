f472b4802d0e082fde78e6c14a172c6ca60ec577
drtrafego <drtrafego@gmail.com>
2025-11-15 20:38:51 -0300

    refactor: remove Meta Pixel e centraliza tags no GTM

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { GoogleTagManager } from '@next/third-parties/google'
-import Script from 'next/script';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,20 +21,6 @@ export default function RootLayout({
         {children}
         <GoogleTagManager gtmId={process.env.NEXT_PUBLIC_GTM_ID} gtmScriptUrl="https://www.googletagmanager.com/gtm.js" />
       </body>
-      <Script id="fb-pixel" strategy="afterInteractive">
-        {`
-          !function(f,b,e,v,n,t,s)
-          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-          n.queue=[];t=b.createElement(e);t.async=!0;
-          t.src=v;s=b.getElementsByTagName(e)[0];
-          s.parentNode.insertBefore(t,s)}(window, document,'script',
-          'https://connect.facebook.net/en_US/fbevents.js');
-          fbq('init', '${process.env.NEXT_PUBLIC_FACEBOOK_PIXEL_ID}');
-          fbq('track', 'PageView');
-        `}
-      </Script>
     </html>
   );
 }
